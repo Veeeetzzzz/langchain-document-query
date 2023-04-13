b6d0c4470f39f4882c0f0684244a2bf387ce82b9
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2023-04-13 16:01:55 +0100

    Updated app.py
    Added functionality to support .DB file uploads

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.sql_database import SQLDatabase
+from langchain.llms.openai import OpenAI
+from langchain.agents import AgentExecutor
 from pydantic import BaseModel
 
 class StringIODocument(BaseModel):
@@ -40,6 +45,8 @@ class StringIOCSVLoader(CSVLoader):
                 content = " ".join([f"{header}: {value}" for header, value in zip(headers, row)])
                 documents.append(CSVDocument(page_content=content))
         return documents
+    
+app = Flask(__name__)    
 
 class PDFLoader:
     def __init__(self, pdf_file):
@@ -48,12 +55,10 @@ class PDFLoader:
     def load(self):
         loader = PyMuPDFLoader(self.pdf_file)
         return loader.load()
-
-app = Flask(__name__)
-
+    
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html")    
 
 @app.route("/ask", methods=["POST"])
 def ask_query():
@@ -61,20 +66,52 @@ def ask_query():
     query = request.form["query"]
     file_ext = request.form["file_ext"]
     pdf_file = request.files.get("upload-pdf")
+    db_file = request.files.get("upload-db")
 
-    if not query or not file_ext:
+    if not query:
         return jsonify({"error": "Invalid input."})
 
-    if file_ext == ".txt":
-        loader = StringIOTextLoader(io.StringIO(content.read().decode("utf-8")))
-    elif file_ext == ".csv":
-        loader = StringIOCSVLoader(io.StringIO(content.read().decode("utf-8")))
+    if file_ext in [".txt", ".csv"]:
+        if file_ext == ".txt":
+            loader = StringIOTextLoader(io.StringIO(content.read().decode("utf-8")))
+        elif file_ext == ".csv":
+            loader = StringIOCSVLoader(io.StringIO(content.read().decode("utf-8")))
+
+        documents = loader.load()
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(documents)
+
+        embeddings = OpenAIEmbeddings()
+        docsearch = Chroma.from_documents(texts, embeddings)
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever(search_kwargs={"k": 1}))
+
+        response = qa.run(query)
     elif pdf_file:
         pdf_filename = secure_filename(pdf_file.filename)
         pdf_file.save(pdf_filename)
         loader = PDFLoader(pdf_filename)
+
+        # Similar processing steps for PDF as for text and csv files
+
+    elif db_file:
+        db_filename = secure_filename(db_file.filename)
+        db_file.save(db_filename)
+        db = SQLDatabase.from_uri(f"sqlite:///{db_filename}")
+        toolkit = SQLDatabaseToolkit(db=db)
+
+        agent_executor = create_sql_agent(
+            llm=OpenAI(temperature=0),
+            toolkit=toolkit,
+            verbose=True
+        )
+
+        response = agent_executor.run(query)
     else:
         return jsonify({"error": "Unsupported file type."})
+
+    return jsonify({"response": response})
+    
+    
         
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
